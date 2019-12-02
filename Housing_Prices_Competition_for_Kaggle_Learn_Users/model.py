e83a691b247f833353891219bba6ced809c91a9f
Roger's-Mac <hzr951110@gmail.com>
2019-12-02 20:54:04 +0800

    seach parameters

--- Housing_Prices_Competition_for_Kaggle_Learn_Users/model.py
+++ Housing_Prices_Competition_for_Kaggle_Learn_Users/model.py
@@ -1,14 +1,12 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import SimpleImputer, IterativeImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from xgboost import XGBRegressor
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, GridSearchCV
 
 
 train_data_path = './data/train.csv'
@@ -17,7 +15,6 @@ test_data_path = './data/test.csv'
 X = pd.read_csv(train_data_path)
 X_test = pd.read_csv(test_data_path)
 
-# print(X_test)
 
 y = X['SalePrice']
 X.drop(['SalePrice'], axis=1, inplace=True)
@@ -37,8 +34,26 @@ preprocessor = ColumnTransformer(
         ('cate', categorical_transformer, categorical_cols),
     ]
 )
-model = XGBRegressor(n_estimators=1000, learning_rate=0.05, objective="reg:squarederror")
 
+
+model = XGBRegressor(objective="reg:squarederror")
+
+gs = GridSearchCV(model, 
+                  {'n_estimators': range(100, 1000, 25), 
+                   'learning_rate': [0.001, 0.003, 0.006, 0.009, 0.03, 0.06, 0.09, 0.1]}, 
+                  cv=5,
+                  scoring='reg_mean_absolute_error',
+                  n_jobs=4,
+                  verbose=1)
+
+gs_pipeline = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('gs', gs)
+])
+
+print(gs.best_params_)
+print(gs.best_score_)
+"""
 pipeline = Pipeline(steps=[
     ('preprocessor', preprocessor),
     ('model', model)
@@ -54,3 +69,4 @@ preds_test = pipeline.predict(X_test)
 output = pd.DataFrame({'Id': X_test.Id,
                        'SalePrice': preds_test})
 output.to_csv('submission.csv', index=False)
+"""
